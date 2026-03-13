c0ec326a604bca90a1fbef45e017dfbd89797b2d
Udit Kumar <ukumar@unraveldata.com>
2026-03-13 12:49:16 +0530

    Correcting the script

--- bigquery-analytics/outputs.tf
+++ bigquery-analytics/outputs.tf
@@ -7,28 +7,3 @@ output "data_exchange_id" {
   description = "The ID of the Data Exchange."
   value       = google_bigquery_analytics_hub_data_exchange.clean_room.data_exchange_id
 }
-
-output "listing_names" {
-  description = "The resource names of the Analytics Hub Listings."
-  value       = { for k, v in google_bigquery_analytics_hub_listing.unravel_listing : k => v.name }
-}
-
-output "listing_ids" {
-  description = "The IDs of the Listings."
-  value       = { for k, v in google_bigquery_analytics_hub_listing.unravel_listing : k => v.listing_id }
-}
-
-output "subscription_name" {
-  description = "The resource name of the Data Exchange subscription."
-  value       = google_bigquery_analytics_hub_data_exchange_subscription.unravel_sub.name
-}
-
-output "subscription_id" {
-  description = "The ID of the Data Exchange subscription."
-  value       = google_bigquery_analytics_hub_data_exchange_subscription.unravel_sub.subscription_id
-}
-
-output "destination_dataset" {
-  description = "The destination dataset of the subscription."
-  value       = google_bigquery_analytics_hub_data_exchange_subscription.unravel_sub.destination_dataset
-}
